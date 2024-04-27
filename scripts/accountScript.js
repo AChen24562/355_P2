1b6bf684ed8d46c4863e32939608604e19f9adda
Alex Chen <alex.chen76@qmail.cuny.edu>
2024-04-27 04:53:43 -0400

    Fix hidden search in account page

--- scripts/accountScript.js
+++ scripts/accountScript.js
@@ -9,3 +9,46 @@ open.addEventListener('click', ()=>
 close.addEventListener('click', ()=>
     container.classList.remove('show-nav')
 )
+
+// Begin hidden Search
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus();
+})
+// End of hidden search
+
+
+// Begin Stock API query
+input.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        getStockQuote(input.value);
+    }
+});
+function getStockQuote(symbol) {
+    if (symbol) {
+        fetch(`/api/quote?symbol=${encodeURIComponent(symbol)}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                sessionStorage.setItem('currentPrice', data.c);
+                sessionStorage.setItem('stockTicker', symbol);
+                // setTimeout(() => window.location.href = '/stock', 100);
+                window.location.href = '/stock';
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+// End of Stock API query
